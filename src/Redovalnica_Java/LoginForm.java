ea72b24d250e5edc28b7e39aa7aeb5e1ad7e73fe
dejan966 <59951914+dejan966@users.noreply.github.com>
2022-01-18 11:57:54 +0100

    Narjen form za prijavo

---
+++ src/Redovalnica_Java/LoginForm.java
@@ -0,0 +1,13 @@
+package Redovalnica_Java;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton prijavaButton;
+
+    public static void main(String[] args) {
+
+    }
+}
