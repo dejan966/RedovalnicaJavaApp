1ee5077d0683a1f5cfa7e5b482c8a02e76a7f046
dejan966 <59951914+dejan966@users.noreply.github.com>
2022-01-20 18:17:37 +0100

    Narjen JPanel za vnašanje ocen + narjen koledar

--- src/Redovalnica_Java/App.java
+++ src/Redovalnica_Java/App.java
@@ -3,6 +3,8 @@ package Redovalnica_Java;
 import com.toedter.calendar.JDateChooser;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App {
     private JPanel panelApp;
@@ -16,8 +18,17 @@ public class App {
     private JButton preveriPrisotnostZaNazajButton;
     private JTree tree1;
     private JPanel jCal;
+    private JButton vnesiOcenoButton;
+    private JButton statistikaZaOceneButton;
+    private JComboBox comboBox5;
+    private JComboBox comboBox6;
+    private JComboBox comboBox7;
+    private JTextField textField2;
+    private JTree tree2;
+    private JPanel jCal2;
 
     JDateChooser chooser = new JDateChooser();
+    JDateChooser chooser2 = new JDateChooser();
     public App(){
         JFrame jframe = new JFrame("Redovalnica");
         jframe.setContentPane(panelApp);
@@ -28,5 +39,10 @@ public class App {
         jframe.setVisible(true);
 
         jCal.add(chooser);
+        jCal2.add(chooser2);
+        statistikaZaOceneButton.addActionListener(e -> {
+            jframe.dispose();
+            new Statistika();
+        });
     }
 }
--- src/Redovalnica_Java/Statistika.java
+++ src/Redovalnica_Java/Statistika.java
@@ -1,4 +1,17 @@
 package Redovalnica_Java;
 
+import javax.swing.*;
+
 public class Statistika {
+    private JPanel jpanel;
+
+    public Statistika(){
+        JFrame jframe = new JFrame("Statistika za ocene");
+        jframe.setContentPane(jpanel);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.pack();
+        jframe.setSize(700, 500);
+        jframe.setResizable(false);
+        jframe.setVisible(true);
+    }
 }
